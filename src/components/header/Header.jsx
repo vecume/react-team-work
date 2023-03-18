a441634b8d87c4d35644f71282d4fc0849b40cb6
Shoha-N1 <abduvohidovs85@gmail.com>
2023-03-18 21:14:16 +0500

    Commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
 import "./Header.scss";
+import img1 from "./../../assets/Logo.svg"
 
 export default function Header() {
   return (
-    <header>Header</header>
-  )
+    <header>
+      <div className="container">
+        <h1>
+          <a href="/">
+            <img src={img1} alt="Logo" />
+          </a>
+        </h1>
+      </div>
+    </header>
+  );
 }
