12c25d919136dfabf230ee90b2f2f46af69e3286
Shoha-N1 <abduvohidovs85@gmail.com>
2023-03-19 20:50:24 +0500

    Commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./Header.scss";
-import img1 from "./../../assets/Logo.svg"
+import img1 from "./../../assets/Logo.svg";
 
 export default function Header() {
+  const [open, setOpen] = useState(false)
+
   return (
     <header>
       <div className="container">
         <div className="header-df">
-        <h1>
-          <a href="/">
-            <img src={img1} alt="Logo" />
-          </a>
-        </h1>
+          <h1>
+            <a href="/">
+              <img src={img1} alt="Logo" />
+            </a>
+          </h1>
 
-        <nav>
-          <ul>
-            <li> <a href="#">Home</a></li>
-            <li> <a href="#">Services</a></li>
-            <li> <a href="#">Portfolio</a></li>
-            <li> <a href="#">About</a></li>
-            <li> <a href="#">Contact</a></li>
-            <li>English</li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li className='menu-trigger' onClick={() => {setOpen(!open)}}>
+                <span>Lang</span>
+                <ul className={`drop-menu ${open ? 'active' : 'inactive'}`}>
+                  <li>
+                    <a href="#">Eng</a>
+                  </li>
+                  <li>
+                    <a href="#">Ru</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
         </div>
-        
       </div>
     </header>
   );
