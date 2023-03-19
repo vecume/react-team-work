42c4dcb06dbcdd281acd49775015617ef274fa9c
Shoha-N1 <abduvohidovs85@gmail.com>
2023-03-19 12:09:33 +0500

    Commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,11 +6,25 @@ export default function Header() {
   return (
     <header>
       <div className="container">
+        <div className="header-df">
         <h1>
           <a href="/">
             <img src={img1} alt="Logo" />
           </a>
         </h1>
+
+        <nav>
+          <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">Services</a></li>
+            <li> <a href="#">Portfolio</a></li>
+            <li> <a href="#">About</a></li>
+            <li> <a href="#">Contact</a></li>
+            <li>English</li>
+          </ul>
+        </nav>
+        </div>
+        
       </div>
     </header>
   );
