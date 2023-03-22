34df3ca220775d994f34063391338c49bad56ba4
kamronvaliyev <kamrovaliyev0805@gmail.com>
2023-03-22 18:34:16 +0300

    add kemran

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/header/Header";
+import Team from "./components/our-team/Team";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />;
+      <Team />
+    </>
+  );
 }
 
 export default App;
