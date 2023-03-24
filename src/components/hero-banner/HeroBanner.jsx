91586ee252f67700f2f512a3d272a7ef057e2ec7
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-24 15:45:52 +0500

    fix buttons array bug inside HeroBanner

--- src/components/hero-banner/HeroBanner.jsx
+++ src/components/hero-banner/HeroBanner.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import PrimaryButton from "../primary-button/PrimaryButton";
 import "./HeroBanner.scss";
 
-export default function HeroBanner({ title, desc, isSmall, buttons }) {
+export default function HeroBanner({ title, desc, isSmall, buttons = "" }) {
   return (
     <section className={`hero-banner ${isSmall ? "hero-banner--small" : ""}`}>
       <div className="hero-banner__content container">
         <h2 className="hero-banner__title">{title}</h2>
         <p className="hero-banner__desc">{desc}</p>
-        {buttons
-          ? buttons.map((item) => (
-              <PrimaryButton
-                key={item.id}
-                text={item.text}
-                isOutline={item.isOutline}
-              />
-            ))
-          : ""}
+        {buttons.map((item) => (
+          <PrimaryButton
+            key={item.id}
+            text={item.text}
+            isOutline={item.isOutline}
+          />
+        ))}
       </div>
     </section>
   );
