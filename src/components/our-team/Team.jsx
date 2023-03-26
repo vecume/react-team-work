77cfea85044fa548eea88b0f5a4c090f7c55f75a
kamronvaliyev <kamrovaliyev0805@gmail.com>
2023-03-26 13:05:32 +0300

    team finished

--- src/components/our-team/Team.jsx
+++ src/components/our-team/Team.jsx
@@ -5,66 +5,114 @@ export default function Team() {
   return (
     <div className="people">
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Sardor Sadatov</h6>
-          <p>Founder & CEO</p>
+        <img src="/sardor.jpg" alt="Sardor" width={"64"} height={"64px"} />
+        <div className="person_info">
+          <h6 className="person_name">Sardor Sadatov</h6>
+          <p className="person_work-name">Founder & CEO</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>SJo‘rayev Ahror</h6>
-          <p>Marketing Director</p>
+        <img
+          className="person_img"
+          src="/firdavs.png"
+          alt="firdavs"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Firdavs Choriyev</h6>
+          <p className="person_work-name">Senior Web Developer</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Sarvarbek Abdupattoyev</h6>
-          <p>Web Designer</p>
+        <img
+          className="person_img"
+          src="/javohir.png"
+          alt="javohir"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Javohir Niyazov</h6>
+          <p className="person_work-name">Web Designer</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Firdavs Choriyev</h6>
-          <p>Senior Web Developer</p>
+        <img
+          className="person_img"
+          src="/ahror.png"
+          alt="ahror"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Jo‘rayev Ahror</h6>
+          <p className="person_work-name">Marketing Director</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Bart Dunweg</h6>
-          <p>Digital Designer</p>
+        <img
+          className="person_img"
+          src="/bart.png"
+          alt="bart"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Bart Dunweg</h6>
+          <p className="person_work-name">Digital Designer</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Dilshod Sattorov</h6>
-          <p>Social Media Specialist</p>
+        <img
+          className="person_img"
+          src="/geluk.png"
+          alt="geluk"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Marysia Geluk</h6>
+          <p className="person_work-name">FOffice Manager</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Javohir Niyazov</h6>
-          <p>Web Designer</p>
+        <img
+          className="person_img"
+          src="/sarvar.png"
+          alt="sarvar"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Sarvarbek Abdupattoyev</h6>
+          <p className="person_work-name">Web Designer</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>SMarysia Geluk</h6>
-          <p>FOffice Manager</p>
+        <img
+          className="person_img"
+          src="/dilshod.png"
+          alt="dilshod"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Dilshod Sattorov</h6>
+          <p className="person_work-name">Social Media Specialist</p>
         </div>
       </div>
       <div className="person">
-        <img src="" alt="Sardor" />
-        <div>
-          <h6>Muzaffar Donayev</h6>
-          <p>Backend Developer</p>
+        <img
+          className="person_img"
+          src="/muzaffar.png"
+          alt="muzaffar"
+          width={"64"}
+          height={"64px"}
+        />
+        <div className="person_info">
+          <h6 className="person_name">Muzaffar Donayev</h6>
+          <p className="person_work-name">Backend Developer</p>
         </div>
       </div>
     </div>
