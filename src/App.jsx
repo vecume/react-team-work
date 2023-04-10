0e0a85066cebf5e0baf89f15319db570621983b6
Akbarov4620 <ogabekakbarov4620@gmail.com>
2023-04-10 15:36:38 +0500

    App tozalandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import "./App.scss";
-import Clients from "./components/our_clients/Clients";
-import Partners from "./components/partners/Partners";
 
 
 function App() {
   return (
     <>
-    <div className="container">
 
-      <Clients/>
-      <Partners/>
-    </div>
     </>
   );
 }
